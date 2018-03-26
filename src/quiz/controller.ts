acadd7582ea2c7ed200aefbbc99834a5be336f14
Billy <b.schrammen@gmail.com>
2018-03-26 18:46:32 +0200

    Merged Delete and get Endpoint

--- src/quiz/controller.ts
+++ src/quiz/controller.ts
@@ -1,11 +1,26 @@
-import { JsonController, Param, BadRequestError, NotFoundError, Get, Body, Patch } from 'routing-controllers'
+import { JsonController, Param, BadRequestError, NotFoundError, Get, Body, Patch, Delete, HttpCode } from 'routing-controllers'
 import { Quiz, Question, Answer } from './entities'
 import { Validate } from 'class-validator'
 
 @JsonController()
 export default class QuizController {
 
-  @Patch('/quiz/:id([0-9]+)')
+  @Get('/quizzes/:id([0-9]+)')
+  @HttpCode(201)
+  getQuiz(@Param('id') quizId: number
+  ) {
+   return Quiz.findOneById(quizId)
+  }
+
+  @Get('/quizzes')
+  @HttpCode(201)
+  getQuizes() {
+   return Quiz.find()
+  }
+
+  //@Authorized()
+  @Patch('/quizzes/:id([0-9]+)')
+  @HttpCode(201)
   async updateQuiz(
     @Param('id') quizId: number,
     @Body() updates
@@ -16,7 +31,21 @@ export default class QuizController {
     await Quiz.merge(quiz, updates).save();
 
     return {
-      message: 'You successfully changed quiz'
+      message: 'You successfully changed the quiz'
     }
   }
+
+   //@Authorized()
+   @Delete('/quizzes/:id([0-9]+)')
+   @HttpCode(201)
+   async deleteQuiz(@Param('id') id: number
+   ) {
+     const quiz = await Quiz.findOneById(id)
+     if (!quiz) throw new NotFoundError(`Quiz does not exist!`)
+     await quiz.remove();
+
+     return {
+       message: "You succesfully deleted the quiz"
+     }
+   }
 }
